3cb1b7f1ad61f3c9b02765c6b13a6785920e3aec
asafnir <asafnirs@gmail.com>
2019-02-06 12:45:21 -0800

    API

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -4,7 +4,7 @@ import _superagent from 'superagent';
 const superagent = superagentPromise(_superagent, global.Promise);
 
 // const API_ROOT = 'http://' + (process.env.API_HOST || 'localhost') + '/api';
-const API_ROOT = 'http://localhost:3000/api';
+const API_ROOT = 'http://relojinout.herokuapp.com/api';
 
 const responseBody = res => res.body;
 
